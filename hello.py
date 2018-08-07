9dae8cd3d7ad033b364cdf8f5a5e01d2d32b5b8e
Muhammad Abdullah <abdullah@SulemanRafi-Desktop.arbisoft.com>
2018-08-07 15:26:16 +0500

    testing

---
+++ hello.py
@@ -0,0 +1,35 @@
+# myint=5
+# print(myint)
+#
+# myfloat=7.0
+# print(myfloat)
+#
+# myfloat2=float(7)
+# print(myfloat2)
+#
+# a, b = 3, 4
+# print(a,b)
+
+#########################################
+
+# mylist=[]
+# mylist.append(1)
+# mylist.append(7.0)
+# mylist.append("hello")
+# mylist[2]=2
+#
+# print(mylist)
+#
+# square=5**2
+# print(square)
+#
+# cube=4**3
+# print(cube)
+
+#########################################
+name = "John"
+names = ["John", "Rick"]
+if name in names:
+    print("Your name is either %s or %s." % (names[0], names[1]))
+else:
+    print("Your name is not in the given list")
